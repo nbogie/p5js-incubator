43b13c126fcd4d922f40098a20f7e43236abd9e2
Neill Bogie <neillbogie@googlemail.com>
2019-04-19 15:01:59 +0100

    finish audio-rippler

--- audio-ripples/js/index.js
+++ audio-ripples/js/index.js
@@ -1,9 +1,9 @@
 "use strict";
 let agents;
-
+let myRippler;
 var mic;
 var amp;
-let gNumAgents = 30;
+let gNumAgents = 20;
 
 //for smoothing
 let smoothingBuffer;
@@ -19,19 +19,19 @@ function setup() {
   amp.setInput(mic);
   smoothingBuffer = new Buffer(4);
   volHistory = new Buffer(100);
-  
+
   createCanvas(windowWidth, windowHeight);
 
   agents = [];
-  
+  myRippler = rippler();
 }
 
-function createAgent(p= randomPos()) {
+function createAgent(p = randomPos()) {
   return {
     pos: p,
     size: 1,
     growSpeed: 1.5,
-    color: color(255,0,0),
+    hue: ((frameCount/10) % 100),
     age: 0,
     phase: random(10)
   }
@@ -56,46 +56,79 @@ function drawAgent(agent) {
   let p = agent.pos;
   push();
   strokeWeight(0.5);
-  let alpha = map(Math.max(100 - agent.age), 0, 200, 0, 100);
-  stroke(color(255, 255, 255, alpha))
+  let alpha = map(Math.max(100 - agent.age), 0, 100, 0, 100);
+  colorMode(HSB, 100)
+  stroke(color(agent.hue, 100, 100, alpha))
   noFill();
-  
+
   circle(p.x, p.y, agent.size);
   pop()
 }
-function updateAgent(agent){
+
+function updateAgent(agent) {
   agent.size += agent.growSpeed;
   agent.age += 1;
 }
 class Buffer {
-  constructor(size){
+  constructor(size) {
     this.buffer = [];
     this.size = size;
   }
   //add a new value to the buffer, removing the oldest element
-  add(val){
+  add(val) {
     this.buffer.push(val);
     this.buffer = this.buffer.slice(-this.size);
   }
 
-  get(ix){
+  get(ix) {
     return this.buffer[ix];
   }
-  
+
   //return value at some position in the buffer, from 0 to 1.  
   // frac: a position float from 0.0 to 1.0
-  getAtFraction(frac){
+  getAtFraction(frac) {
     let ix = floor(this.buffer.length * frac);
     return this.get(ix);
   }
-  
+
   //return an average of all of the values in the buffer  
-  average(){
+  average() {
     return this.buffer.reduce((accum, current) => accum + current) / this.size;
   }
 }
-function createRipple(agents){
-  agents.push(createAgent({ x: mouseX, y: mouseY }));
+
+function jitter(pos, v){
+  return {
+    x: pos.x + random(-v, v),
+    y: pos.y + random(-v, v),
+  }
+}
+function mousePos(){
+  return {x: mouseX, y: mouseY};
+}
+
+function createRipple(agents) {
+  agents.push(createAgent(jitter(mousePos(), 4)));
+}
+
+function rippler() {
+  let lastRippleTime = millis()
+
+  return {
+    stimulate: function(agents){
+      let now = millis();
+      if (now - lastRippleTime > 100){
+        createRipple(agents);
+        lastRippleTime = millis();
+      }
+    }
+  }
+
+}
+function cullAgents(){
+  if (agents.length > gNumAgents){
+    agents = agents.slice(-gNumAgents);
+  }
 }
 
 function draw() {
@@ -106,10 +139,10 @@ function draw() {
   let avg = smoothingBuffer.average();
 
   volHistory.add(avg);
-  textSize(32);
-  if (avg > 0.3){
-    createRipple(agents);
-  } 
+  if (amp.getLevel() > 0.3) {
+    myRippler.stimulate(agents);
+  }
   agents.forEach(updateAgent);
   agents.forEach(drawAgent);
+  cullAgents()
 }
