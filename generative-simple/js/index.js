4f69f2dc4c1857614196688ec499a418509fd50a
Neill Bogie <neillbogie@googlemail.com>
2019-07-06 21:11:47 +0100

    triangle petals, labelled palettes, more

--- generative-simple/js/index.js
+++ generative-simple/js/index.js
@@ -1,39 +1,97 @@
 "use strict";
+const seedHistory = [];
+let showDebug = false;
 let shouldDrawPalettePreview = false;
-
-const THING_RADIUS = 60;
+let gSeed = 0;
+let THING_RADIUS = 50;
 
 const FaveColors = {
   paletteStrs: [
-    "#F8B195,#F67280,#C06C84,#6C5B7B,#355C7D,#F8B195,#F67280,#C06C84", //1001 stories http://www.colourlovers.com/palette/1811244/1001_Stories
-    "#5E412F,#FCEBB6,#78C0A8,#F07818,#F0A830,#5E412F,#FCEBB6,#78C0A8", //papua new guinea http://www.colourlovers.com/palette/919313/Papua_New_Guinea
-    "#452632,#91204D,#E4844A,#E8BF56,#E2F7CE,#452632,#91204D,#E4844A", //trance http://www.colourlovers.com/palette/594151/t_r_a_n_c_e
-    "#F0D8A8,#3D1C00,#86B8B1,#F2D694,#FA2A00,#F0D8A8,#3D1C00,#86B8B1", //koi carp http://www.colourlovers.com/palette/656966/Koi_Carp
-    "#FF4E50,#FC913A,#F9D423,#EDE574,#E1F5C4,#FF4E50,#FC913A,#F9D423", //dance to forget http://www.colourlovers.com/palette/937624/Dance_To_Forget
-    "#99B898,#FECEA8,#FF847C,#E84A5F,#2A363B,#99B898,#FECEA8,#FF847C", //coup de grace http://www.colourlovers.com/palette/1098589/coup_de_gr%C3%A2ce
-    "#FF4242,#F4FAD2,#D4EE5E,#E1EDB9,#F0F2EB,#FF4242,#F4FAD2,#D4EE5E", //wasabi suicide http://www.colourlovers.com/palette/482416/Wasabi_Suicide
-    "yellow,yellow,gray",
-    "#c70000,#f4b600,#2d2bb4,black",
-    "#FE4365,#FC9D9A,#F9CDAD,#C8C8A9,#83AF9B,#FE4365,#FC9D9A,#F9CDAD",
-    "#69D2E7,#A7DBD8,#E0E4CC,#F38630,#FA6900,#69D2E7,#A7DBD8,#E0E4CC",
-    "#556270,#4ECDC4,#C7F464,#FF6B6B,#C44D58,#556270,#4ECDC4,#C7F464",
-    "#E94E77,#D68189,#C6A49A,#C6E5D9,#F4EAD5", // ["LoversInJapan by lovelyrita"](http://www.colourlovers.com/palette/867235/LoversInJapan)
-    "#00A0B0,#6A4A3C,#CC333F,#EB6841,#EDC951", // ["Ocean Five by DESIGNJUNKEE"](http://www.colourlovers.com/palette/1473/Ocean_Five)
-    "#B9D7D9,#668284,#2A2829,#493736,#7B3B3B", // "Entrapped InAPalette by annajak",
-    "#D1F2A5,#EFFAB4,#FFC48C,#FF9F80,#F56991", // "mellon ball surprise by Skyblue2u"
-    "#00A8C6,#40C0CB,#F9F2E7,#AEE239,#8FBE00" // "fresh cut day by electrikmonk"
+    "#F8B195,#F67280,#C06C84,#6C5B7B,#355C7D,#F8B195,#F67280,#C06C84|1001 stories|http://www.colourlovers.com/palette/1811244/1001_Stories",
+    "#5E412F,#FCEBB6,#78C0A8,#F07818,#F0A830,#5E412F,#FCEBB6,#78C0A8|papua new guinea|http://www.colourlovers.com/palette/919313/Papua_New_Guinea",
+    "#452632,#91204D,#E4844A,#E8BF56,#E2F7CE,#452632,#91204D,#E4844A|trance|http://www.colourlovers.com/palette/594151/t_r_a_n_c_e",
+    "#F0D8A8,#3D1C00,#86B8B1,#F2D694,#FA2A00,#F0D8A8,#3D1C00,#86B8B1|koi carp|http://www.colourlovers.com/palette/656966/Koi_Carp",
+    "#FF4E50,#FC913A,#F9D423,#EDE574,#E1F5C4,#FF4E50,#FC913A,#F9D423|dance to forget|http://www.colourlovers.com/palette/937624/Dance_To_Forget",
+    "#99B898,#FECEA8,#FF847C,#E84A5F,#2A363B,#99B898,#FECEA8,#FF847C|coup de grace|http://www.colourlovers.com/palette/1098589/coup_de_gr%C3%A2ce",
+    "#FF4242,#F4FAD2,#D4EE5E,#E1EDB9,#F0F2EB,#FF4242,#F4FAD2,#D4EE5E|wasabi suicide|http://www.colourlovers.com/palette/482416/Wasabi_Suicide",
+    "yellow,yellow,gray||",
+    "#c70000,#f4b600,#2d2bb4,black||",
+    "black,gray,white||",
+    "white||",
+    "#FE4365,#FC9D9A,#F9CDAD,#C8C8A9,#83AF9B,#FE4365,#FC9D9A,#F9CDAD||",
+    "#69D2E7,#A7DBD8,#E0E4CC,#F38630,#FA6900,#69D2E7,#A7DBD8,#E0E4CC||",
+    "#556270,#4ECDC4,#C7F464,#FF6B6B,#C44D58,#556270,#4ECDC4,#C7F464||",
+    "#E94E77,#D68189,#C6A49A,#C6E5D9,#F4EAD5|LoversInJapan by lovelyrita|http://www.colourlovers.com/palette/867235/LoversInJapan",
+    "#00A0B0,#6A4A3C,#CC333F,#EB6841,#EDC951|Ocean Five by DESIGNJUNKEE|http://www.colourlovers.com/palette/1473/Ocean_Five",
+    "#B9D7D9,#668284,#2A2829,#493736,#7B3B3B|Entrapped InAPalette by annajak|",
+    "#D1F2A5,#EFFAB4,#FFC48C,#FF9F80,#F56991|mellon ball surprise by Skyblue2u|",
+    "#00A8C6,#40C0CB,#F9F2E7,#AEE239,#8FBE00|fresh cut day by electrikmonk|"
   ],
-  randomPalette: () =>
-    random(FaveColors.paletteStrs)
-      .split(",")
-      .map(n => color(n))
+  randomPalette: function() {
+    const makePalette = str => {
+      const [colorsStr, name, url] = str.split("|");
+      return {
+        colors: colorsStr.split(",").map(n => color(n)),
+        name: name,
+        url: url
+      };
+    };
+
+    const palettes = FaveColors.paletteStrs.map(makePalette);
+    return random(palettes);
+  },
+  randomMonoPalette: function() {
+    const pal = Object.assign({}, FaveColors.randomPalette());
+    pal.colors = _.sample(pal.colors, 2);
+    return pal;
+  }
 };
 
 function randomizePalette() {
   gPalette = FaveColors.randomPalette();
 }
+function randomizeMonoPalette() {
+  gPalette = FaveColors.randomMonoPalette();
+}
 
 let gPalette;
+
+function keyPressed() {
+  switch (key) {
+    case "d":
+      toggleShowDebug();
+      break;
+    case " ":
+      newRandomSeed();
+      redraw();
+      break;
+    case "p":
+      prevRandomSeed();
+      redraw();
+      break;
+    case "r":
+      randomizePalette();
+      redraw();
+      break;
+    case "m":
+      randomizeMonoPalette();
+      redraw();
+      break;
+  }
+}
+
+function newRandomSeed() {
+  seedHistory.push(gSeed);
+  gSeed = Math.floor(random(0, 999999));
+  randomSeed(gSeed);
+}
+
+function prevRandomSeed() {
+  if (seedHistory.length > 0) {
+    gSeed = seedHistory.pop();
+    randomSeed(gSeed);
+  }
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -48,7 +106,7 @@ function repeat(n, fn) {
   }
 }
 function randomColor() {
-  return random(gPalette);
+  return random(gPalette.colors);
 }
 function randomPos() {
   return { x: random(width), y: random(height) };
@@ -70,7 +128,7 @@ function drawSquare() {
   rotate(random([0, PI / 4]));
   fill(randomColorOrTransparent());
   stroke(randomColor());
-  strokeWeight(10);
+  strokeWeight(1);
   square(0, 0, random(50, 100));
   pop();
 }
@@ -78,15 +136,19 @@ function drawSquare() {
 function drawPetals(petalFn, extentScale) {
   push();
   const sz = random(10, 40);
-  fill(random([randomColor(), createEmptyColor()]));
-  stroke(randomColor());
-  strokeWeight(2);
+
+  noFill();
+  noStroke();
+  random([true, false]) ? fill(randomColor()) : stroke(randomColor());
+  strokeWeight(1);
   angleMode(RADIANS);
   const numPetals = random([6, 8]);
+  const startRotation = TWO_PI / numPetals / 2;
   repeat(numPetals, ix => {
     push();
-    rotate((ix * TWO_PI) / numPetals);
+    rotate(startRotation + (ix * TWO_PI) / numPetals);
     translate(0, THING_RADIUS - (extentScale * sz) / 2);
+    //rotate(PI / 4);
     petalFn(sz);
     pop();
   });
@@ -96,6 +158,16 @@ function drawPetals(petalFn, extentScale) {
 function drawSquarePetals() {
   drawPetals(sz => square(0, 0, sz), 1.21);
 }
+function drawTrianglePetals() {
+  const rotationAmt = random([PI, 0]);
+  drawPetals(sz => {
+    const d = sz / 2;
+    push();
+    rotate(rotationAmt);
+    triangle(-d, -d, d, -d, 0, d);
+    pop();
+  }, 1);
+}
 function drawCirclePetals() {
   drawPetals(sz => circle(0, 0, sz / 2), 1);
 }
@@ -103,7 +175,7 @@ function drawCirclePetals() {
 function drawConcentricCircles() {
   const maxRad = THING_RADIUS * 0.9;
   noFill();
-  strokeWeight(random([2, 10]));
+  strokeWeight(random([1, 1, 1, 10]));
   repeat(random([4, 6, 8]), (ix, lim) => {
     stroke(randomColor());
     circle(0, 0, (ix * maxRad) / lim);
@@ -112,13 +184,21 @@ function drawConcentricCircles() {
 function randomColorOrTransparent() {
   return random([randomColor(), createEmptyColor()]);
 }
-function drawHexagon() {
-  push();
+function drawSteppedHexagons() {
+  repeat(random([4, 6, 8, 10]), (ix, lim) => {
+    drawHexagon((ix / lim) * THING_RADIUS, false);
+  });
+}
+function drawOuterHexagon() {
   const radius = THING_RADIUS;
+  drawHexagon(radius, random([false, false, false, true]));
+}
+function drawHexagon(radius, filled = false) {
   const sliceAngle = TWO_PI / 6;
-  const startAngle = 0;
-  strokeWeight(2);
-  fill(randomColorOrTransparent());
+  const startAngle = filled ? PI / 6 : 0;
+  push();
+  strokeWeight(1);
+  fill(filled ? randomColor() : createEmptyColor());
   stroke(randomColor());
   beginShape();
   repeat(6, ix => {
@@ -132,11 +212,13 @@ function drawHexagon() {
 function drawRadialLines() {
   const numLines = random([6, 4, 8, 12]);
   const sliceAngle = TWO_PI / numLines;
-  const endRadius = THING_RADIUS;
-  const startRadius = random([1, 2, 3, 4, 5]) * 10;
+  const startRadiusStep = random([1, 2, 3, 4, 5]);
+  const endRadiusStep = startRadiusStep + random(1, 6 - startRadiusStep);
+  const endRadius = (endRadiusStep * THING_RADIUS) / 6;
+  const startRadius = (startRadiusStep * THING_RADIUS) / 6;
 
   noFill();
-  strokeWeight(2);
+  strokeWeight(1);
   stroke(randomColor());
   push();
   repeat(numLines, ix => {
@@ -146,19 +228,40 @@ function drawRadialLines() {
   pop();
 }
 
+function mouseDragged() {
+  THING_RADIUS = map(mouseX, 0, width, 10, 60);
+  redraw();
+}
+
+function textLabel(msg, x, y) {
+  const padding = 30;
+  const h = textAscent(msg) + textDescent(msg);
+  const w = textWidth(msg);
+  fill(color(255, 230)); //"white");
+  strokeWeight(2);
+  stroke("#404040");
+  rect(x + w / 2, y - textDescent(msg), w + padding, h + padding, 5, 5);
+  noStroke();
+  fill("#404040");
+  text(msg, x, y);
+}
 function draw() {
-  background("white");
-  const numRows = 4;
-  const numCols = 6;
-  drawGridOfThings(numRows, numCols, drawThing);
-  //  drawGridOfThings(numRows, numCols, drawHexagon);
+  background("whitesmoke");
+  const numRows = height / (6 * THING_RADIUS + 2);
+  const numCols = width / (6 * THING_RADIUS + 2);
+  drawGridOfThings(numRows * 2, numCols * 2, drawThing);
+  if (showDebug) {
+    textSize(24);
+    textLabel(gPalette.name || "untitled", 100, height * 0.8);
+    textLabel("seed: " + gSeed, 100, height * 0.91);
+  }
 }
 
 function drawGridOfThings(numRows, numCols, drawThingFn) {
   const ySpacing = height / numRows;
   const xSpacing = width / numCols;
-  const yStart = 80;
-  const xStart = 80;
+  const xStart = THING_RADIUS * 1.5;
+  const yStart = THING_RADIUS * 1.5;
 
   for (let row = 0; row < numRows; row++) {
     const y = yStart + row * ySpacing;
@@ -172,21 +275,13 @@ function drawGridOfThings(numRows, numCols, drawThingFn) {
   }
 }
 
-function maybe(fn) {
-  Math.random() > 0.5 && fn();
+function randInt(min, max) {
+  return Math.floor(random(min, max + 1));
 }
 
-function atLeastOneOf(fns) {
-  const shuffledFns = _.shuffle(fns);
-  if (fns) {
-    const maxChoicePacked = Math.pow(2, fns.length);
-    let choicesPacked = int(random(1, maxChoicePacked));
-    shuffledFns.forEach(f => {
-      const include = choicesPacked & 1;
-      include && f();
-      choicesPacked = choicesPacked >> 1;
-    });
-  }
+function atLeastTwoOf(fns) {
+  const pickedFns = _.sample(fns, randInt(2, fns.length));
+  pickedFns.forEach(f => f());
 }
 
 function drawGuideLines() {
@@ -197,13 +292,16 @@ function drawGuideLines() {
 }
 
 function drawThing() {
-  //drawGuideLines();
-
-  atLeastOneOf([
+  // drawGuideLines();
+  atLeastTwoOf([
     //    x => repeat(random(1, 2), drawSquare),
-    random([drawSquarePetals, drawCirclePetals]),
-    drawHexagon,
-    drawRadialLines,
-    drawConcentricCircles
+    random([drawSquarePetals, drawCirclePetals, drawTrianglePetals]),
+    drawOuterHexagon,
+    random([drawSteppedHexagons, drawConcentricCircles]),
+    drawRadialLines
   ]);
+}
+
+function toggleShowDebug() {
+  showDebug = !showDebug;
 }
